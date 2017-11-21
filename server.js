566691049726df18fdf647769c3e4415a6e75b89
pompeypigeon <pompeypigeon@gmail.com>
2017-11-21 12:37:31 +0000

    Added a few things
    Tried to implement ws, may not work :)

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require("express");
 var http = require("http");
 var websocket = require("ws");
-var app = express();
-var server = http.createServer(app);
-var wss = new websocket.Server({server});
 
-/*wss.on('connection', (ws : WebSocket) => {
-	ws.on('message', (message : string) =>{
-		ws.send("Hi");
-	});
-});*/
+var server = http.createServer();
+var wss = new wsserver({server:server});
+var app = express();
+
+server.on('request', app);
+
+function broadcastWS(message){
+	wss.clients.forEach(function(client){
+		if(client.readyState === client.OPEN){
+			client.send(message);
+		}
+	})
+}
+
+function respondWS(ws){
+	ws.on('message', broadcastWS);
+}
 
 app.get('/', function(req,res){
 	res.sendFile('pages/index.html', {root:__dirname});
 })
 
-app.listen(3000);
+wss.on('connection', respondWS);
+
+app.listen(8080);
