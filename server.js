bcd4cf7f5fd709aaf6db078903d38c63917cf264
pompeypigeon <pompeypigeon@gmail.com>
2017-11-28 13:20:27 +0000

    Added functionality to speech recognition
    Removed websockets stuff for now, think I will need to get help for those as I cannot get them to work...

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
-/*var express = require("express");
-var http = require("http").Server(app);
-var app = express();
-var io = require("socket.io")(http);
-
-app.get('/', function(req,res){
-	res.sendFile('pages/index.html', {root:__dirname});
-});
-
-io.on('connection', function(socket){
-	console.log('User connected.');
-});
-
-app.listen(8080);*/
-
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
-var socket = require("socket.io");
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname+"/pages/index.html");
 })
 
-var io = socket.listen(app.listen(8080));
-
-io.on('connection', function(socket){
-	console.log("connection on", socket.id);
-})
+app.listen(8080);
