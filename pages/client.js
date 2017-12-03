e4ab8e183592a200927dbdbbdab0921909a780c0
pompeypigeon <pompeypigeon@gmail.com>
2017-12-03 17:21:28 +0000

    Sockets now transmitting speech
    Server can receive and transmit the transcript. Now rooms could be implemented if needed?

--- pages/client.js
+++ pages/client.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var socket = io();
+
 //all query selectors
 var	addFont = 		document.querySelector('#addFont'),
 	lessFont = 		document.querySelector('#lessFont'),
@@ -12,6 +16,8 @@ var	addFont = 		document.querySelector('#addFont'),
 var sr = 			new webkitSpeechRecognition(),
 	finalResult = 	'',
 	started = 		false;
+
+
  
 sr.continuous = true; //speech input is continuous, no need to start it all the time
 sr.interimResults = true; //enables interim, on the fly, results
@@ -19,10 +25,7 @@ sr.interimResults = true; //enables interim, on the fly, results
 final.addEventListener('click', finalTranscript);
 start.addEventListener('click', startSpeech);
 
-/*
- * =============================================================
- * Transcript
- */
+
 function finalTranscript(){
 	alert(finalResult);
 }
@@ -47,12 +50,11 @@ function startSpeech(event){
 		
 		//when audio has been detected
 		sr.onaudiostart = function(){
-			status.textContent = 'Capturing';
+			//status.textContent = 'Capturing';
 		};
 				
 		//once audio input has finished.
 		sr.onend = function(){
-			console.log('AyyLmaonyez');
 			started = false;
 		};
 		
@@ -68,36 +70,56 @@ function startSpeech(event){
 				};
 			};
 			
-			line.textContent = interimResult;
+			/*send that to server
+			socket.emit('subtitles', {
+				text:interimResult
+			});*/
+			socket.emit('send text', interimResult);
 		};
 	};
 };
 
+socket.on('text', function(data){
+	line.textContent = data.txt;
+})
 
 /*
  * =================================================================
  * Subtitle box stuff
  */
 
-//increase font size to subtitle box
+//increase font size in subtitle box
 addFont.addEventListener('click', function(){
-	line.style.fontSize = window.getComputedStyle(line, null).getPropertyValue('font-size') + 1;
+	var currentFontSize = parseInt(window.getComputedStyle(line, null).getPropertyValue('font-size'));
+	line.style.fontSize = currentFontSize + 2 ;
 });
 
-//reduce font size
+//reduce font size  in subtitle box
 lessFont.addEventListener('click', function(){
-	
+	var currentFontSize = parseInt(window.getComputedStyle(line, null).getPropertyValue('font-size'));
+	line.style.fontSize = currentFontSize - 2 ;
 });
 
-subtitle.addEventListener('mousedown', mouseDown);
+//change foreground colour of subtitles
+foreColour.addEventListener('change', function(){
+	line.style.color = foreColour.options[foreColour.selectedIndex].text;
+});
 
+//change background colour of subtitles
+backColour.addEventListener('change', function(){
+	line.style.backgroundColor = backColour.options[backColour.selectedIndex].text;
+});
+
+//move subtitle box when user clicks on subtitles
+line.addEventListener('mousedown', function(){
+	window.addEventListener('mousemove', moveSubtitles);
+});
+
+//when user lets go of mouse, stop moving
 window.addEventListener('mouseup', function(){
 	window.removeEventListener('mousemove', moveSubtitles)
 });
 
-function mouseDown(event){
-	window.addEventListener('mousemove', moveSubtitles);
-}
 function moveSubtitles(){
 	subtitle.style.position = 'absolute';
 	subtitle.style.top = event.clientY + 'px';
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-const	express = require('express'),
+/*const	express = require('express'),
 		app = express(),
 		server = require('http').Server(app),
 		io = require('socket.io')(server),
@@ -7,5 +7,45 @@ const	express = require('express'),
 
 app.use(express.static(__dirname + '/pages'));
 
+/*function onConnect(socket){
+	console.log("A connection")
+	socket.on('subtitles', function(data){
+		socket.broadcast.emit('subtitles', data);
+		console.log(data);
+	})
+};
+
+io.on('connection', function(socket){
+	console.log("a conn");
+});
+
 server.listen(port);
 console.log("On port " + port);
+
+*/
+
+const	express = 	require('express'),
+		app = 		express(),
+		server = 	require('http').Server(app),
+		io = 		require('socket.io').listen(server),
+		port = 		process.env.PORT || 8080;
+	
+server.listen(8080, function(){
+	console.log("On port 8080");
+});
+console.log("server running");
+
+app.use(express.static(__dirname + '/pages'));
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname, "/pages/index.html");
+});
+
+io.sockets.on('connection', function(socket){
+	console.log("Connected");
+	socket.on('send text', function(data){
+		console.log(data);
+		io.sockets.emit('text', {txt: data});
+	});
+});
+
